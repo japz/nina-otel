9fedd72e540224b4e3328b2757ea2f52abd37a5c
x0r <x0r@noreply.x0r.li>
2026-06-14 15:41:10 +0200

    fix: add plugin assembly metadata

---
+++ src/NinaOtel.Plugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NinaOtel")]
+[assembly: AssemblyDescription("OpenTelemetry foundation plugin for NINA.")]
+[assembly: AssemblyCompany("NinaOtel")]
+[assembly: AssemblyProduct("NinaOtel")]
+[assembly: AssemblyCopyright("Copyright (c) 2026")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-foundation")]
+[assembly: Guid("7311D7B5-0BBD-45AB-8E29-DB09B289A798")]
